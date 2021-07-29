91c83517eb0bc23b3064cca925fdf39f3133d011
yunseho <79745084+yunseho@users.noreply.github.com>
2021-07-29 16:02:42 +0900

    히어로시즈

---
+++ gameNode.h
@@ -0,0 +1,35 @@
+#pragma once
+#include"image.h"
+
+static image* backBuffer = IMAGE->addImage("backbuffer", WINSIZEX, WINSIZEY);
+static image* screenbuffer = IMAGE->addImage("screenbuffer", WINSIZEX*2, WINSIZEY);
+
+class gameNode
+{
+
+	//image* _backBuffer;	//백버퍼
+	//void setBackBuffer();//백버퍼 셋팅
+
+	HDC _hdc;
+	bool  _managerInit;
+
+public:
+	gameNode();
+	virtual~gameNode();
+
+	virtual HRESULT init();
+	virtual HRESULT init(bool managerInit);
+	virtual void release();
+	virtual void update();
+	virtual void render(/*HDC hdc*/);
+
+	image* getBackBuffer()const { return backBuffer; }
+	HDC getHDC()const { return _hdc; }
+	HDC getMemDC()const { return backBuffer->getMemDC(); }
+
+	image* getScreenBuffer()const { return screenbuffer; }
+	HDC getScreenDC()const { return screenbuffer->getMemDC(); }
+
+	LRESULT MainProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+};
---
+++ mainGame.h
@@ -0,0 +1,19 @@
+#pragma once
+#include"gameNode.h"
+
+class mainGame : public gameNode
+{
+private:
+	
+
+public:
+	mainGame();
+	~mainGame();
+
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render();
+
+
+};
