9d9af60ca6647ac04b6e92da82369ff87b11280f
LarJi <87188826+LarJi@users.noreply.github.com>
2021-07-30 15:12:47 +0900

    Cimage 일부작업
    Cimage 일부작업

--- Cimage.cpp
+++ Cimage.cpp
@@ -32,11 +32,22 @@ HRESULT Cimage::init()
 
     //=================Object=================//
 
+    IMAGE->addFrameImage("≥™π´∫Ú", "images/≥™π´∫Ú.bmp", 1134, 242, 6, 1, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("≥™π´Ω∫∏Ù", "images/≥™π´Ω∫∏Ù.bmp", 1520, 182, 10, 1, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("≥™π´¿Â¿€", "images/≥™π´¿Â¿€.bmp", 308, 75, 4, 1, true, RGB(255, 0, 255));
 
 
 
     //=================item=================//
-    IMAGE->addFrameImage("Ω≈πﬂ", "images/Ω≈πﬂ.bmp", 384, 320, 6, 5, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("Ω≈πﬂ∫Ú", "images/Ω≈πﬂ∫Ú.bmp", 384, 320, 7, 4, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("Ω≈πﬂΩ∫∏Ù", "images/Ω≈πﬂΩ∫∏Ù.bmp", 224, 128, 7, 4, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("∞©ø ∫Ú", "images/∞©ø ∫Ú.bmp", 528, 528, 6, 6, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("∞©ø Ω∫∏Ù", "images/∞©ø Ω∫∏Ù.bmp", 264, 264, 6, 6, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("π´±‚∫Ú", "images/π´±‚∫Ú.bmp", 623, 111, 7, 1, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("∆Ê¥¯∆Æ∫Ú", "images/∆Ê¥¯∆Æ∫Ú.bmp", 384, 256, 6, 4, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("∆Ê¥¯∆ÆΩ∫∏Ù", "images/∆Ê¥¯∆ÆΩ∫∏Ù.bmp", 210, 140, 6, 4, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("¿Â∞©∫Ú", "images/¿Â∞©∫Ú.bmp", 576, 192, 9, 3, true, RGB(255, 0, 255));
+    IMAGE->addFrameImage("¿Â∞©Ω∫∏Ù", "images/¿Â∞©Ω∫∏Ù.bmp", 288, 96, 9, 3, true, RGB(255, 0, 255));
 
 
 
