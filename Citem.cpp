02984b9d7d0a469dbae0edb03f288e3865df6f71
steidl90 <87983461+steidl90@users.noreply.github.com>
2021-08-08 15:40:54 +0900

    아이템생성연습

--- Citem.cpp
+++ Citem.cpp
@@ -1,2 +1,40 @@
 #include "framework.h"
 #include "Citem.h"
+
+Citem::Citem()
+{
+}
+
+Citem::~Citem()
+{
+}
+
+HRESULT Citem::init()
+{
+	return S_OK;
+}
+
+void Citem::release()
+{
+}
+
+void Citem::update()
+{
+}
+
+void Citem::render()
+{
+}
+
+void Citem::load()
+{
+    HANDLE file;
+    DWORD read;
+
+    file = CreateFile("tileMap.map",
+        GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    /*ReadFile(file, m_itemKind, sizeof(item), &read, NULL);
+    ReadFile(file, m_itemType, sizeof(item), &read, NULL);*/
+    CloseHandle(file);
+}
--- Citem.h
+++ Citem.h
@@ -1,5 +1,39 @@
 #pragma once
-class Citem
+#include "Cobject.h"
+
+enum class ITEMKIND
 {
+	ITEMKIND_ONE,
+	ITEMKIND_TWO
+};
+
+enum class ITEMTYPE
+{
+	ITEMTYPE_ONE,
+	ITEMTYPE_TWO,
+	ITEMTYPE_THREE
+};
+
+struct item
+{
+	ITEMKIND itemKind;
+	ITEMTYPE itemType;
+};
+
+class Citem : public Cobject
+{
+private:
+	item item;
+
+public:
+	Citem();
+	~Citem();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	void load();
 };
 
