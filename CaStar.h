863ca75fe9b27ea613605de9bab9f7208c63202d
GDtiger <86643952+GDtiger@users.noreply.github.com>
2021-08-09 20:38:38 +0900

    주석

--- CaStar.h
+++ CaStar.h
@@ -1,38 +1,38 @@
 #pragma once
 #include "gameNode.h"
 
-#define TILE_X 20
+#define TILE_X 50
 #define TILE_Y 20
 #define WIDTH  35
 #define HEIGHT 35
 #define BIGNUM 5000
 
-enum class TILE_TYPE
+enum class TILE_TYPE		//타일속성
 {
-	TILE_TYPE_EMPTY,
-	TILE_TYPE_START,
-	TILE_TYPE_END,
-	TILE_TYPE_WALL
+	TILE_TYPE_EMPTY,		//갈수있는곳
+	TILE_TYPE_START,		//몬스터위치
+	TILE_TYPE_END,			//플레이어 위치
+	TILE_TYPE_WALL			//벽
 };
 
 enum class ASTAR_STATE
 {
-	ASTAR_STATE_SEARCHING,
-	ASTAR_STATE_FOUND,
-	ASTAR_STATE_NOWAY,
+	ASTAR_STATE_SEARCHING,		//검색중
+	ASTAR_STATE_FOUND,			//발견
+	ASTAR_STATE_NOWAY,			//못가는곳
 	ASTAR_STATE_END
 };
 struct aStarTile
 {
 	RECT rc;
 	COLORREF color;
-	bool walkable;		
-	bool listOn;		
+	bool walkable;				//지나갈수 있는가
+	bool listOn;				//오픈리스트에 추가되었는지 여부
 	int i, j;
 	int F, G, H;
 	char str[128];
-	aStarTile* parent;	
-	TILE_TYPE type;		
+	aStarTile* parent;			//부모타일 주소
+	TILE_TYPE type;				//타일 속성
 };
 
 class CaStar :public gameNode
