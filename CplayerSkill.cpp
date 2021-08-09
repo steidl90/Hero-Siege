c7f1168e07efeef5ca4da7991f8c652543426021
LarJi <87188826+LarJi@users.noreply.github.com>
2021-08-09 16:52:16 +0900

    플레이어 스킬 구현중

--- CplayerSkill.cpp
+++ CplayerSkill.cpp
@@ -11,6 +11,8 @@ CplayerSkill::~CplayerSkill()
 
 HRESULT CplayerSkill::init()
 {
+    skillDirection = SKILL::SKILL_DOWN;
+
     return S_OK;
 }
 
@@ -23,5 +25,68 @@ void CplayerSkill::update()
 }
 
 void CplayerSkill::render()
+{
+}
+
+void CplayerSkill::skillQ()
+{
+	skillBoxRc = RectMake();
+    if (InputManager->isOnceKeyDown('Q'))
+    {
+		if (InputManager->isStayKeyDown(VK_RIGHT) && (InputManager->isStayKeyDown(VK_UP) || InputManager->isStayKeyDown(VK_DOWN)))
+		{
+			
+			skillDirection = SKILL::SKILL_RIGHT;
+		}
+		else if (InputManager->isStayKeyDown(VK_LEFT) && (InputManager->isStayKeyDown(VK_UP) || InputManager->isStayKeyDown(VK_DOWN)))
+		{
+			
+			skillDirection = SKILL::SKILL_LEFT;
+		}
+		else if (InputManager->isStayKeyDown(VK_UP) && (InputManager->isStayKeyDown(VK_RIGHT) || InputManager->isStayKeyDown(VK_LEFT)))
+		{
+			
+			skillDirection = SKILL::SKILL_UP;
+		}
+		else if (InputManager->isStayKeyDown(VK_DOWN) && (InputManager->isStayKeyDown(VK_RIGHT) || InputManager->isStayKeyDown(VK_LEFT)))
+		{
+			skillDirection = SKILL::SKILL_DOWN;
+		}
+		else if (InputManager->isStayKeyDown(VK_LEFT))
+		{
+			skillDirection = SKILL::SKILL_LEFT;
+		}
+		else if (InputManager->isStayKeyDown(VK_RIGHT))
+		{
+			skillDirection = SKILL::SKILL_RIGHT;
+		}
+		else if (InputManager->isStayKeyDown(VK_UP))
+		{
+			skillDirection = SKILL::SKILL_UP;
+		}
+		else if (InputManager->isStayKeyDown(VK_DOWN))
+		{
+			skillDirection = SKILL::SKILL_DOWN;
+		}
+    }
+    switch (skillDirection)
+    {
+    case SKILL::SKILL_LEFT:
+        break;
+    case SKILL::SKILL_UP:
+        break;
+    case SKILL::SKILL_RIGHT:
+        break;
+    case SKILL::SKILL_DOWN:
+        break;
+
+    }
+}
+
+void CplayerSkill::skillW()
+{
+}
+
+void CplayerSkill::skillE()
 {
 }
--- CplayerSkill.h
+++ CplayerSkill.h
@@ -1,9 +1,18 @@
 #pragma once
 #include "Cunit.h"
+
+enum class  SKILL
+{
+	SKILL_LEFT,
+	SKILL_UP,
+	SKILL_RIGHT,
+	SKILL_DOWN
+};
 class CplayerSkill :public Cunit
 {
 private:
-
+	SKILL skillDirection;
+	RECT skillBoxRc;
 public:
 	CplayerSkill();
 	~CplayerSkill();
@@ -12,5 +21,8 @@ public:
 	void release();
 	void update();
 	void render();
+	void skillQ();
+	void skillW();
+	void skillE();
 };
 
