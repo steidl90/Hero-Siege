0a89536775254c3cef332dd4b228c5d826672f6f
LarJi <87188826+LarJi@users.noreply.github.com>
2021-08-13 15:50:36 +0900

    보스????
    기본 공격 대충??? 씬 마을에서 던전으로 변경

--- Cmevius.cpp
+++ Cmevius.cpp
@@ -12,6 +12,8 @@ Cmevius::~Cmevius()
 HRESULT Cmevius::init()
 {
     m_em = new CenemyManager;
+    //m_meviusImage = IMAGE->findImage("보스");
+    m_meviusAttackImage = IMAGE->findImage("애니공");
 	ANIMATION->addDefAnimation("애니공중", "보스공중", 10, false, true);
     ANIMATION->addDefAnimation("애니보스", "보스", 5, false, true);
     ANIMATION->addDefAnimation("애니캐스팅", "보스캐스팅", 10, false, false);
@@ -20,6 +22,8 @@ HRESULT Cmevius::init()
     ANIMATION->addDefAnimation("애니공", "보스공", 10, false, true);
     EFFECT->addEffect("라이트닝", "images/Lightning.bmp", 576, 402, 72, 402, 1, 0.25f, 100);
     EFFECT->addEffect("스텀프", "images/Stomp.bmp", 819*2, 78*2, 91*2, 78*2, 1, 0.2f, 200);
+
+    m_meviusAnimation = ANIMATION->findAnimation("애니보스");
 
     m_isAppear = true;
     m_isEffect = false;
@@ -32,6 +36,8 @@ HRESULT Cmevius::init()
     m_x = WINSIZEX / 2;
     m_y = -50;
     m_speed = 2;
+  
+   // m_meviusRc = RectMake(m_x, m_y, m_meviusImage->getFrameWidth(), m_meviusImage->getFrameHeight());
     return S_OK;
 }
 
@@ -42,79 +48,22 @@ void Cmevius::release()
 
 void Cmevius::update()
 {
-   if (m_isAppear)
-   {
-       m_isLevitating = true;
-       m_isEffect = true;
-       m_isAppear = false;
-   }
-   
-   if (m_isEffect)
-   {
-       m_effectCount++;
-     if (m_effectCount % 4 == 0)
-     {
-         EFFECT->play("라이트닝", RND->getFromIntTo(100, WINSIZEX - 100), RND->getFromIntTo(100, WINSIZEY - 100));
-     }
-   }
-
-    if (m_isLevitating)
-    {
-        m_meviusImage = IMAGE->findImage("보스공중");
-        m_meviusAnimation = ANIMATION->findAnimation("애니공중");
-        ANIMATION->start("애니공중");
-        if (m_meviusRc.top <= 400) m_y += m_speed;
-		else
-		{
-			m_coolTime++;
-			if (m_coolTime == 50)
-			{
-				m_isEffect = false;
-                m_isLevitating = false;
-				m_isWalking = true;
-                m_isIdle = true;
-			EFFECT->play("스텀프", m_meviusRc.left + (m_meviusRc.right - m_meviusRc.left) / 2, m_meviusRc.bottom);
-                //m_coolTime = 0;
-			}
-		}
-	}
-	if (m_isWalking)
-	{
-		m_meviusImage = IMAGE->findImage("보스걷기");
-		m_meviusAnimation = ANIMATION->findAnimation("애니걷기");
-		ANIMATION->start("애니걷기");
-		m_isWalking = false;
-	}
-
-	if (!m_isAppear && !m_isWalking && m_meviusRc.top <= 450)
-	{
-		m_y += 0.2f;
-	}
-
-    if (m_meviusRc.top >= 450 && m_isIdle)
-    {
-        m_meviusImage = IMAGE->findImage("보스");
-        m_meviusAnimation = ANIMATION->findAnimation("애니보스");
-        ANIMATION->start("애니보스");
-        m_isIdle = false;
-        m_isCasting = true;
-    }
-
+    m_bossAngle + 0.4;
+    appear();
     if (m_isCasting && InputManager->isOnceKeyDown('3'))
     {
         m_meviusImage = IMAGE->findImage("보스캐스팅");
         m_meviusAnimation = ANIMATION->findAnimation("애니캐스팅2");
         ANIMATION->start("애니캐스팅2");
-
+        defaultState();
     }
 
-    if (InputManager->isOnceKeyDown('2'))
-    {
-        init();
-    }
+    
     if (m_meviusImage != nullptr) {
         m_meviusRc = RectMake(m_x, m_y, m_meviusImage->getFrameWidth(), m_meviusImage->getFrameHeight());
     }
+    bossFireMove("애니공");
+
 }
 
 void Cmevius::render()
@@ -126,10 +75,145 @@ void Cmevius::render()
     //wsprintf(str1, "idle : %d walk : %d  casting : %d", m_isIdle,m_isWalking,m_isCasting);
     //TextOut(getMemDC(), 100, 130, str1, lstrlen(str1));
     if (m_meviusImage != nullptr) {
+        Rectangle(getMapDC(), m_meviusRc.left, m_meviusRc.top, m_meviusRc.right, m_meviusRc.bottom);
         m_meviusImage->aniRender(getMapDC(), m_x, m_y, m_meviusAnimation);
         EFFECT->render();
+    }
+    bossFireRender();
+}
+
+void Cmevius::bossFire(float x, float y, float angle, float speed, float range, const char* fileName, const char* aniName)
+{
+    tagBossAttack bossAttack;
+    ZeroMemory(&bossAttack, sizeof(tagPlayerSkill));
+    bossAttack.m_bossAttackImage = new image;
+    bossAttack.m_bossAttackImage = IMAGE->findImage(fileName);
+    bossAttack.m_bossAttackAni = ANIMATION->findAnimation(aniName);
+    bossAttack.m_angle = angle;
+    bossAttack.m_speed = speed;
+    bossAttack.m_attackX = bossAttack.m_fireX = x;
+    bossAttack.m_attackY = bossAttack.m_fireY = y;
+    bossAttack.m_range = range;
+    bossAttack.m_attackRc = RectMakeCenter(bossAttack.m_attackX, bossAttack.m_attackY, bossAttack.m_bossAttackImage->getFrameWidth(), bossAttack.m_bossAttackImage->getFrameHeight());
+    m_vBossAttack.push_back(bossAttack);
+}
+
+void Cmevius::bossFireMove(const char* aniName)
+{
+    for (m_viBossAttack = m_vBossAttack.begin(); m_viBossAttack != m_vBossAttack.end();)
+    {
+        m_viBossAttack->m_fireX += cosf(m_viBossAttack->m_angle) * m_viBossAttack->m_speed;
+        m_viBossAttack->m_fireY -= sinf(m_viBossAttack->m_angle) * m_viBossAttack->m_speed;
+
+        m_viBossAttack->m_attackRc = RectMakeCenter(m_viBossAttack->m_fireX, m_viBossAttack->m_fireY,
+            m_viBossAttack->m_bossAttackImage->getFrameWidth(),
+            m_viBossAttack->m_bossAttackImage->getFrameHeight());
+
+        if (m_viBossAttack->m_range < UTIL::getDistance(m_viBossAttack->m_fireX, m_viBossAttack->m_fireY, m_viBossAttack->m_attackX, m_viBossAttack->m_attackY))
+        {
+            m_viBossAttack = m_vBossAttack.erase(m_viBossAttack);
+        }
+        else ++m_viBossAttack;
+    }
+    ANIMATION->findAnimation(aniName);
+    ANIMATION->resume(aniName);
+}
+
+void Cmevius::bossFireRender()
+{
+    for (m_viBossAttack = m_vBossAttack.begin(); m_viBossAttack != m_vBossAttack.end(); ++m_viBossAttack)
+    {
+        Rectangle(getMapDC(), m_viBossAttack->m_attackRc.left, m_viBossAttack->m_attackRc.top, m_viBossAttack->m_attackRc.right, m_viBossAttack->m_attackRc.bottom);
+        m_viBossAttack->m_bossAttackImage->aniRender(getMapDC(), m_viBossAttack->m_attackRc.left, m_viBossAttack->m_attackRc.top, m_viBossAttack->m_bossAttackAni);
 
     }
+}
+
+void Cmevius::appear()
+{
+    if (m_isAppear)
+    {
+        m_isLevitating = true;
+        m_isEffect = true;
+        m_isAppear = false;
+    }
+
+    if (m_isEffect)
+    {
+        m_effectCount++;
+        if (m_effectCount % 4 == 0)
+        {
+            EFFECT->play("라이트닝", RND->getFromIntTo(100, WINSIZEX - 100), RND->getFromIntTo(100, WINSIZEY - 100));
+        }
+    }
+
+    if (m_isLevitating)
+    {
+        m_meviusImage = IMAGE->findImage("보스공중");
+        m_meviusAnimation = ANIMATION->findAnimation("애니공중");
+        ANIMATION->start("애니공중");
+        if (m_meviusRc.top <= 400) m_y += m_speed;
+        else
+        {
+            m_coolTime++;
+            if (m_coolTime == 50)
+            {
+                m_isEffect = false;
+                m_isLevitating = false;
+                m_isWalking = true;
+                m_isIdle = true;
+                EFFECT->play("스텀프", m_meviusRc.left + (m_meviusRc.right - m_meviusRc.left) / 2, m_meviusRc.bottom);
+                //m_coolTime = 0;
+            }
+        }
+    }
+    if (m_isWalking)
+    {
+        m_meviusImage = IMAGE->findImage("보스걷기");
+        m_meviusAnimation = ANIMATION->findAnimation("애니걷기");
+        ANIMATION->start("애니걷기");
+        m_isWalking = false;
+    }
+
+    if (!m_isAppear && !m_isWalking && m_meviusRc.top <= 450)
+    {
+        m_y += 0.2f;
+    }
+
+    if (m_meviusRc.top >= 450 && m_isIdle)
+    {
+        m_meviusImage = IMAGE->findImage("보스");
+        m_meviusAnimation = ANIMATION->findAnimation("애니보스");
+        ANIMATION->start("애니보스");
+        m_isIdle = false;
+        m_isCasting = true;
+    }
+}
+
+void Cmevius::defaultState()
+{
+    for (size_t i = 0; i < 15; i++)
+    {
+        bossFire(m_meviusRc.left + (m_meviusRc.right - m_meviusRc.left) / 2, m_meviusRc.top + (m_meviusRc.bottom - m_meviusRc.top) / 2,(m_bossAngle+i)*0.42, 5.0f, 400, "보스공", "애니공");
+        bossFire(m_meviusRc.left + (m_meviusRc.right - m_meviusRc.left) / 2, m_meviusRc.top + (m_meviusRc.bottom - m_meviusRc.top) / 2, PI*(m_bossAngle + i) * 0.24, 4.0f, 400, "보스공", "애니공");
+        bossFire(m_meviusRc.left + (m_meviusRc.right - m_meviusRc.left) / 2, m_meviusRc.top + (m_meviusRc.bottom - m_meviusRc.top) / 2, PI*1.5*(m_bossAngle + i) * 0.2, 3.0f, 400, "보스공", "애니공");
+
+    }
+
+}
+
+
+void Cmevius::phase1()
+{
+
+}
+
+void Cmevius::phase2()
+{
+}
+
+void Cmevius::phase3()
+{
 }
 
 void Cmevius::meviusState()
@@ -165,7 +249,6 @@ void Cmevius::meviusState()
     if (m_effectCount % 4 == 0)
     {
         EFFECT->play("라이트닝", RND->getFromIntTo(100, WINSIZEX - 100), RND->getFromIntTo(100, WINSIZEY - 100));
-        //EFFECT->play("라이트닝", RND->getFromIntTo(100, WINSIZEX - 100), RND->getFromIntTo(100, WINSIZEY - 100));
     }
 
 
--- Cmevius.h
+++ Cmevius.h
@@ -1,6 +1,21 @@
 #pragma once
 #include "Cunit.h"
 #include "CenemyManager.h"
+struct tagBossAttack
+{
+	image* m_bossAttackImage;
+	RECT m_attackRc;
+	animation* m_bossAttackAni;
+
+	float m_attackX, m_attackY;
+	float m_fireX, m_fireY;
+	float m_radius;
+	float m_angle;
+	float m_speed;
+	float m_range;
+
+	bool m_fire;
+};
 enum class BOSS_STATE
 {
 	BOSS_STATE_IDLE,
@@ -12,10 +27,15 @@ enum class BOSS_STATE
 class Cmevius :public Cunit
 {
 private:
+	vector<tagBossAttack> m_vBossAttack;
+	vector<tagBossAttack>::iterator m_viBossAttack;
+
 	image* m_meviusImage;
 	animation* m_meviusAnimation;
 	effect* m_meviusEffect;
 	BOSS_STATE m_meviusState;
+
+	image* m_meviusAttackImage;
 
 	CenemyManager* m_em;
 
@@ -26,6 +46,8 @@ private:
 
 	float m_x, m_y;
 	float m_speed;
+	float m_time;
+	float m_bossAngle;
 
 	float m_hp;
 	float m_skillDamagePattern1;
@@ -51,9 +73,22 @@ public:
 	void release();
 	void update();
 	void render();
+	void bossFire(float x, float y, float angle, float speed, float range, const char* fileName, const char* aniName);
+	void bossFireMove(const char* aniName);
+	void bossFireRender();
+	void appear();
+	void defaultState();
+	void phase1();
+	void phase2();
+	void phase3();
+
+
+
 
 	void meviusState();
 	void coolTime(float time,bool idle, bool walk, bool cast);
 
+	vector<tagBossAttack> getvBossAttack() { return m_vBossAttack; };
+	vector<tagBossAttack>::iterator getviBossAttack() { return m_viBossAttack; }
 };
 
